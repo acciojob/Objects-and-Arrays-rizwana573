37be0c0c829e5e4d1767eff240dabbeaecbb23b3
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-02 22:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+let team = players[2];
+let team1 = players.clone();
+let cap1 = person;
 
 window.players = players;
 window.person = person;
